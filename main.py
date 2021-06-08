444d6c78df1919c4ecad1e3c48ca5aea83c1da16
xanderwasserman <XANDERWASSERMAN@GMAIL.COM>
2021-06-08 22:29:44 +0200

    Complete

--- main.py
+++ main.py
@@ -16,19 +16,23 @@ def roberts(image):
     m, n = image.shape
     roberts = np.zeros((m, n))
 
-    for i in range(2, m - 1):
-        for j in range(2, n - 1):
-            x = np.square(roberts_x[0, 0] * image[i - 1, j - 1] + roberts_x[0, 1] * image[i - 1, j] + roberts_x[1, 0] * image[i, j - 1] + roberts_x[1, 1] * image[i, j])
-            y = np.square( roberts_y[0, 0] * image[i - 1, j - 1] + roberts_y[0, 1] * image[i - 1, j] + roberts_y[1, 0] * image[i, j - 1] + roberts_y[1, 1] * image[i, j])
-            roberts[i,j] = np.sqrt(x+y)
+    for i in range(2, m):
+        for j in range(2, n):
+            img = image[i - 1:i + 1, j - 1:j + 1]
+            x = roberts_x * img
+            r_x = np.square(x.sum())
+            y = roberts_y * img
+            r_y = np.square(y.sum())
+            roberts[i, j] = np.sqrt(r_x + r_y)
 
     for i in range(m):
         for j in range(n):
-            if roberts[i,j]>50: #was 127
-                roberts[i,j]=255
+            if roberts[i, j] > 127:
+                roberts[i, j] = 255
             else:
-                roberts[i,j]=0
+                roberts[i, j] = 0
     return roberts
+
 
 def prewitt(image):
     # Prewitt mask
@@ -42,15 +46,16 @@ def prewitt(image):
         [-1, 0, 1]])
 
     m, n = image.shape
-
     prewitt = np.zeros((m, n))
 
-    for i in range(2, m - 1):
-        for j in range(2, n - 1):
-            roberts[i, j] = prewitt_x[0, 0] * image[i - 1, j - 1] + prewitt_x[0, 1] * image[i - 1, j] + prewitt_x[
-                0, 2] * image[i - 1, j + 1] + prewitt_x[1, 0] * image[i, j - 1] + prewitt_x[1, 1] * image[i, j] + \
-                            prewitt_x[1, 2] * image[i, j + 1] + prewitt_x[2, 0] * image[i + 1, j - 1] + prewitt_x[
-                                2, 1] * image[i + 1, j] + prewitt_x[2, 2] * image[i + 1, j + 1]
+    for i in range(1, m - 1):
+        for j in range(1, n - 1):
+            img = image[i-1:i+2, j-1:j+2]
+            x = prewitt_x*img
+            r_x = np.square(x.sum())
+            y = prewitt_y*img
+            r_y = np.square(y.sum())
+            prewitt[i, j] = np.sqrt(r_x + r_y)
 
     for i in range(m):
         for j in range(n):
@@ -59,6 +64,7 @@ def prewitt(image):
             else:
                 prewitt[i, j] = 0
     return prewitt
+
 
 def sobel(image):
     # Sobel mask
@@ -72,22 +78,43 @@ def sobel(image):
         [-1, 0, 1]])
 
     m, n = image.shape
+    sobel = np.zeros((m, n))
+
+    for i in range(2, m - 1):
+        for j in range(2, n - 1):
+            img=image[i - 1:i + 2, j - 1:j + 2]
+            x = sobel_x * img
+            r_x = np.square(x.sum())
+            y = sobel_y * img
+            r_y = np.square(y.sum())
+            sobel[i, j] = np.sqrt(r_x + r_y)
+
+    for i in range(m):
+        for j in range(n):
+            if sobel[i, j] > 127:
+                sobel[i, j] = 255
+            else:
+                sobel[i, j] = 0
+    return sobel
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    image = cv2.imread('C:\workspaces/PhotogrammetryWorkspace/test1.jpg', 0)
-    image = cv2.resize(image, (800, 800))
+    #image = cv2.imread('C:\workspaces/PhotogrammetryWorkspace/test1.jpg', 0)
+    #image = cv2.imread('C:\workspaces/PhotogrammetryWorkspace/test2.jpg', 0)
+    image = cv2.imread('C:\workspaces/PhotogrammetryWorkspace/test3.jpg', 0)
+
+    image = cv2.resize(image, (720,720))
     cv2.imshow('Original Image', image)
 
     roberts_image = roberts(image)
-    #prewitt_image = prewitt(image)
-    #sobel_image = sobel(image)
+    prewitt_image = prewitt(image)
+    sobel_image = sobel(image)
 
-    image = cv2.resize(image, (800, 600))
-    roberts_sharpened = cv2.resize(roberts_image, (800, 600))
-    cv2.imshow('Roberts Sharpened', roberts_sharpened)
+    cv2.imshow('Roberts Edges', roberts_image)
+    cv2.imshow('Prewitt Edges', prewitt_image)
+    cv2.imshow('Sobel Edges', sobel_image)
 
     if cv2.waitKey(0) & 0xFF == 27:
         cv2.destroyAllWindows()
