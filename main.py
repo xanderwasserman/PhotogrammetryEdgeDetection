d72f1e6701fcaa8861e85aa7354b76b66f1fd503
xanderwasserman <61466759+xanderwasserman@users.noreply.github.com>
2021-06-16 12:19:31 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import numpy as np
 import cv2
-import threading
 
 def roberts(image, threshold):
     # Roberts mask
@@ -13,28 +12,31 @@ def roberts(image, threshold):
         [1, 0]
     ])
 
-    m, n = image.shape
-    roberts = np.zeros((m, n))
+    m, n = image.shape # get dimensions of image
+    roberts = np.zeros((m, n)) # allocates space for the new image array
 
-    for i in range(2, m):
-        for j in range(2, n):
-            img = image[i - 1:i + 1, j - 1:j + 1]
-            x = roberts_x * img
-            r_x = np.square(x.sum())
-            y = roberts_y * img
+    for i in range(1, m): # loops through image x-axis position(i, j) is the bottom right hand coner of the mask
+        for j in range(1, n):
+            img = image[i - 1:i + 1, j - 1:j + 1] # saves the values of the respective pixels underneath the mask
+
+            x = roberts_x * img # multiply the roberts mask values by their respective pixel values
+            r_x = np.square(x.sum()) # squares the sum of the mask response
+
+            y = roberts_y * img # process is repeated for the second roberts mask
             r_y = np.square(y.sum())
+            # the roberts response is the sqrt of the square of the sum of the pixel responses
             roberts[i, j] = np.sqrt(r_x + r_y)
 
-    for i in range(m):
+    for i in range(m): # applies the pixel response threshold to the resulting image.
         for j in range(n):
-            if roberts[i, j] > threshold:
+            if roberts[i, j] > threshold:  # if the response is above the threshold then the value is changed to 255
                 roberts[i, j] = 255
-            else:
+            else: # if the response is below the threshold then the value is changed to 0
                 roberts[i, j] = 0
 
-    return roberts
+    return roberts # returns the new image of the response
 
-
+# similar process is applied here than for the roberts operator, but just with a different mask
 def prewitt(image, threshold):
     # Prewitt mask
     prewitt_x = np.array([
@@ -67,7 +69,7 @@ def prewitt(image, threshold):
 
     return prewitt
 
-
+# similar process is applied here than for the roberts operator, but just with a different mask
 def sobel(image, threshold):
     # Sobel mask
     sobel_x = np.array([
@@ -103,42 +105,34 @@ def sobel(image, threshold):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    image1 = cv2.imread('C:\workspaces/PhotogrammetryWorkspace/test1.jpg', 0)
-    image2 = cv2.imread('C:\workspaces/PhotogrammetryWorkspace/test2.jpg', 0)
-    image3 = cv2.imread('C:\workspaces/PhotogrammetryWorkspace/test3.jpg', 0)
+    image = cv2.imread('C:\workspaces/PhotogrammetryWorkspace/test1.jpg', 0) #opening of image using opencv
 
-    image1 = cv2.resize(image1, (720,720))
-    image2 = cv2.resize(image2, (720, 720))
-    image3 = cv2.resize(image3, (720, 720))
+    image = cv2.resize(image, (720,720)) #resizing image using opencv in order to speed up processing
 
-    threshold = 200
+    threshold = 127 #edge detection threshold
 
-    roberts_image1 = roberts(image1, threshold)
-    roberts_image2 = roberts(image2, threshold)
-    roberts_image3 = roberts(image3, threshold)
+    # function calls
+    roberts_image = roberts(image, threshold)
+    prewitt_image = prewitt(image, threshold)
+    sobel_image = sobel(image, threshold)
 
-    prewitt_image1 = prewitt(image1, threshold)
-    prewitt_image2 = prewitt(image2, threshold)
-    prewitt_image3 = prewitt(image3, threshold)
+    # display and save original image (post resizing)
+    cv2.imshow('Original Test Image', image)
+    cv2.imwrite('Test_Image_1.jpg', image)
 
-    sobel_image1 = sobel(image1, threshold)
-    sobel_image2 = sobel(image2, threshold)
-    sobel_image3 = sobel(image3, threshold)
+    # show edge detection results
+    cv2.imshow('Roberts_Edges_Test_T' + str(threshold), roberts_image)
+    cv2.imshow('Prewitt_Edges_Test_T' + str(threshold), prewitt_image)
+    cv2.imshow('Sobel_Edges_Test_T' + str(threshold), sobel_image)
 
-    cv2.imwrite('Test_Image_1.jpg', image1)
-    cv2.imwrite('Test_Image_2.jpg', image2)
-    cv2.imwrite('Test_Image_3.jpg', image3)
+    # write edge detection results to file
+    cv2.imwrite('Roberts_Edges_Test_T' + str(threshold) + '.jpg', roberts_image)
+    cv2.imwrite('Prewitt_Edges_Test_T' + str(threshold) + '.jpg', prewitt_image)
+    cv2.imwrite('Sobel_Edges_Test_T' + str(threshold) + '.jpg', sobel_image)
 
-    cv2.imwrite('Roberts_Edges_Test_1_T200.jpg', roberts_image1)
-    cv2.imwrite('Roberts_Edges_Test_2_T200.jpg', roberts_image2)
-    cv2.imwrite('Roberts_Edges_Test_3_T200.jpg', roberts_image3)
-
-    cv2.imwrite('Prewitt_Edges_Test_1_T200.jpg', prewitt_image1)
-    cv2.imwrite('Prewitt_Edges_Test_2_T200.jpg', prewitt_image2)
-    cv2.imwrite('Prewitt_Edges_Test_3_T200.jpg', prewitt_image3)
-
-    cv2.imwrite('Sobel_Edges_Test_1_T200.jpg', sobel_image1)
-    cv2.imwrite('Sobel_Edges_Test_2_T200.jpg', sobel_image2)
-    cv2.imwrite('Sobel_Edges_Test_3_T200.jpg', sobel_image3)
 
     print('COMPLETED')
+
+    # shows edge detection results until keyboard input
+    if cv2.waitKey(0) & 0xFF == 27:
+        cv2.destroyAllWindows()
